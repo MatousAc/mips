a2a23f609c56cb879a5764994cada95ba622b76c
MatousAc <matoush@southern.edu>
2021-11-30 22:29:17 -0500

    processor entity

---
+++ processor/processor.vhd
@@ -0,0 +1,16 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity processor is
+port(
+	reset					: in	std_logic;
+	slow_clk, fast_clk: in 	std_logic;
+	PC_out, inst_out	: out	std_logic_vector(31 downto 0);
+	read_reg1_out		: out std_logic_vector(4 downto 0);
+	read_reg2_out		: out std_logic_vector(4 downto 0);
+	write_reg_out		: out std_logic_vector(4 downto 0);
+	read_data1_out		: out std_logic_vector(31 downto 0);
+	read_data2_out		: out std_logic_vector(31 downto 0);
+	write_data_out		: out std_logic_vector(31 downto 0);
+);
+end processor;
